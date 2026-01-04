ad2ebb9b986acff18c7c772b23b016732d87d537
Stefan Lörwald <10850250+stefanloerwald@users.noreply.github.com>
2026-01-04 16:58:35 +0100

    Add debug log for serial begin in setup
    Added debug log for serial initialization in setup.

--- components/axp2101/axp2101.cpp
+++ components/axp2101/axp2101.cpp
@@ -83,6 +83,7 @@ void AXP2101Component::setup()
 {
     ESP_LOGD(TAG, "setup");
     Serial.begin(115200);
+    ESP_LOGD(TAG, "setup: serial begin called");
     
     bool result = power.begin(Wire, AXP2101_SLAVE_ADDRESS, i2c_sda, i2c_scl);
 
